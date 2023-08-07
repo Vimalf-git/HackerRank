d9805a7ac26f5a1547ab3e7befc98d0f518a62b4
vimal raj <109568312+Vimalf-git@users.noreply.github.com>
2023-08-07 23:17:08 +0530

    Update LeftRotation.java

--- LeftRotation.java
+++ LeftRotation.java
@@ -1,25 +1,41 @@
+import java.io.*;
+import java.util.*;
 
-/*Traditional way to solve*/
- Scanner sc=new Scanner(System.in);
+public class Solution {
+    
+    
+    
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc=new Scanner(System.in);
         String[] d=sc.nextLine().split(" ");                
         String[] arr=sc.nextLine().split(" ");
         int len=Integer.parseInt(d[0]);
         int rot=Integer.parseInt(d[1]);
-        //System.out.println(len+" "+rot);
-
-        //String[] result=new String[len];
-        for(int i=1;i<=rot;i++){
-            String temp=arr[0];
-            for(int j=0;j<len-1;j++){
-                
-                arr[j]=arr[j+1];
-            }
-            arr[len-1]=temp;
-        }
-        
+        /* Using algo solve problem efficient time-comlexity:log(n^2)*/
+        String[] result=new String[len];
         for(int i=0;i<len;i++){
-            System.out.print(arr[i]+" ");
+            int index=(i+len-rot)%len;
+            result[index]=arr[i];
         }
+        for(int j=0;j<len;j++){
+            
+        System.out.print(result[j]+" ");
+        }
+     /* Traditional method to solve time-comlexity:log(n)*/
+        // for(int i=1;i<=rot;i++){
+        //     String temp=arr[0];
+        //     for(int j=0;j<len-1;j++){
+                
+        //         arr[j]=arr[j+1];
+        //     }
+        //     arr[len-1]=temp;
+        // }
+        
+        // for(int i=0;i<len;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
 
-/* Using effective algorithm*/
-
+            }
+}
